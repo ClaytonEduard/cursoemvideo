29193258ab95c8e226a4e52d97da85b24d0e9980
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-26 12:34:06 -0300

    Aula 11 finalizada

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2;
+console.log(` A velocidade dos seu carro é ${vel}Km/h`);
+if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Dirija sem usando cinto de segurança');
