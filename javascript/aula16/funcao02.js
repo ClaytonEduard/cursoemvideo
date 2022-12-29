e949014a945eca6e5b3317da1139d1312204ce18
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-29 10:07:42 -0300

    Aula 16 funcao com paramentro adcional 0

---
+++ javascript/aula16/funcao02.js
@@ -0,0 +1,8 @@
+//function somar
+// paramentro opcional =0, caso nao passe o segundo numero como paramento, e utilizado o 0
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2)) // utilizando o parametro 0
+console.log(soma(2,5)) // sem o paramentro 0
